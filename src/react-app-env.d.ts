603bf5064c47b3c440f4cc778451fc88c1a50748
Thanh Tien <vothanhtien190901@gmail.com>
2023-04-06 19:18:05 +0700

    update

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,13 @@
-/// <reference types="react-scripts" />
-    
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "*.pdf";
+declare module "*.avif";
+declare module "*.glb";
+declare namespace NodeJS {
+  interface ProcessEnv {
+    //types of envs
+    NODE_ENV: "development" | "production" | "test";
+    PUBLIC_URL: string;
+  }
+}
